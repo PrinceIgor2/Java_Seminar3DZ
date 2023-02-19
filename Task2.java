36e1948e127221b399e110decd71e796f4fa9ca7
Igor <kugimn@list.ru>
2023-02-19 10:39:58 +0300

    2+

---
+++ Task2.java
@@ -0,0 +1,19 @@
+/*  1) Пусть дан произвольный список целых чисел, удалить из него четные числа  */
+
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Task2 {
+    
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(ThreadLocalRandom.current().nextInt(1, 30));
+        }
+        System.out.print("Начальный список: ");
+        System.out.println(list);
+        System.out.print("Финальный список: ");
+        list.removeIf(integer -> integer % 2 == 0);
+        System.out.println(list);
+    }
+}
